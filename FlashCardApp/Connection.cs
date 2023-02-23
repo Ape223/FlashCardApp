9daf5f7e9afa8e3fe2aa6b35908f42643a32e42c
hlu98 <hlu98@LAPTOP-NQAIHG8R>
2023-02-23 19:11:03 +0000

    Test

--- FlashCardApp/Connection.cs
+++ FlashCardApp/Connection.cs
@@ -13,7 +13,7 @@ namespace FlashCardApp
     {
         public SqlConnection connection = new SqlConnection();
         public SqlCommand cmd = new SqlCommand();
-        public string location = @"Data Source = (LocalDB)\MSSQLLocalDB; AttachDbFilename = C:\Users\hlu98\source\repos\Flash-Capp-10-42\FlashCardApp (1)\FlashCardApp\Users.mdf; Integrated Security = True";
+        public string location = @"Data Source = (LocalDB)\MSSQLLocalDB; AttachDbFilename = C:\Users\hlu98\Desktop\FlashCardApp (1)\FlashCardApp\Users.mdf; Integrated Security = True";
         //https://www.youtube.com/watch?v=_S-LacqE7OM
     }
 }
--- FlashCardApp/CreateFlashcard.Designer.cs
+++ FlashCardApp/CreateFlashcard.Designer.cs
@@ -33,14 +33,6 @@
             this.label1 = new System.Windows.Forms.Label();
             this.loadDeck = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
-            this.button5 = new System.Windows.Forms.Button();
-            this.button6 = new System.Windows.Forms.Button();
-            this.button7 = new System.Windows.Forms.Button();
-            this.button8 = new System.Windows.Forms.Button();
-            this.button9 = new System.Windows.Forms.Button();
             this.label3 = new System.Windows.Forms.Label();
             this.term = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
@@ -51,15 +43,14 @@
             this.removeCard = new System.Windows.Forms.Button();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
             // 
             // saveDeck
             // 
-            this.saveDeck.Font = new System.Drawing.Font("Segoe UI", 14.25F);
+            this.saveDeck.Font = new System.Drawing.Font("Yu Mincho", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.saveDeck.Location = new System.Drawing.Point(577, 12);
             this.saveDeck.Name = "saveDeck";
-            this.saveDeck.Size = new System.Drawing.Size(99, 34);
+            this.saveDeck.Size = new System.Drawing.Size(99, 38);
             this.saveDeck.TabIndex = 0;
             this.saveDeck.Text = "Save";
             this.saveDeck.UseVisualStyleBackColor = true;
@@ -68,19 +59,19 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Segoe UI", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(3, 8);
+            this.label1.Font = new System.Drawing.Font("Yu Mincho", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(9, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(461, 50);
+            this.label1.Size = new System.Drawing.Size(468, 48);
             this.label1.TabIndex = 1;
             this.label1.Text = "Create Your Flashcard deck";
             // 
             // loadDeck
             // 
-            this.loadDeck.Font = new System.Drawing.Font("Segoe UI", 12F);
-            this.loadDeck.Location = new System.Drawing.Point(88, 67);
+            this.loadDeck.Font = new System.Drawing.Font("Yu Mincho", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.loadDeck.Location = new System.Drawing.Point(119, 58);
             this.loadDeck.Name = "loadDeck";
-            this.loadDeck.Size = new System.Drawing.Size(555, 32);
+            this.loadDeck.Size = new System.Drawing.Size(555, 39);
             this.loadDeck.TabIndex = 2;
             this.loadDeck.Text = "Load your deck here";
             this.loadDeck.UseVisualStyleBackColor = true;
@@ -90,104 +81,25 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Segoe UI", 21.75F);
-            this.label2.Location = new System.Drawing.Point(10, 64);
+            this.label2.Location = new System.Drawing.Point(12, 57);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(85, 40);
             this.label2.TabIndex = 3;
             this.label2.Text = "Deck:";
             // 
-            // button2
-            // 
-            this.button2.Font = new System.Drawing.Font("Segoe UI", 14.25F);
-            this.button2.Location = new System.Drawing.Point(327, 120);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(27, 32);
-            this.button2.TabIndex = 5;
-            this.button2.Text = "Save";
-            this.button2.UseVisualStyleBackColor = true;
-            // 
-            // button3
-            // 
-            this.button3.Font = new System.Drawing.Font("Segoe UI", 14.25F);
-            this.button3.Location = new System.Drawing.Point(285, 123);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(27, 32);
-            this.button3.TabIndex = 6;
-            this.button3.Text = "Save";
-            this.button3.UseVisualStyleBackColor = true;
-            // 
-            // button4
-            // 
-            this.button4.Font = new System.Drawing.Font("Segoe UI", 14.25F);
-            this.button4.Location = new System.Drawing.Point(246, 120);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(27, 32);
-            this.button4.TabIndex = 7;
-            this.button4.Text = "Save";
-            this.button4.UseVisualStyleBackColor = true;
-            // 
-            // button5
-            // 
-            this.button5.Font = new System.Drawing.Font("Segoe UI", 14.25F);
-            this.button5.Location = new System.Drawing.Point(214, 120);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(27, 32);
-            this.button5.TabIndex = 8;
-            this.button5.Text = "Save";
-            this.button5.UseVisualStyleBackColor = true;
-            // 
-            // button6
-            // 
-            this.button6.Font = new System.Drawing.Font("Segoe UI", 14.25F);
-            this.button6.Location = new System.Drawing.Point(183, 120);
-            this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(27, 32);
-            this.button6.TabIndex = 9;
-            this.button6.Text = "Save";
-            this.button6.UseVisualStyleBackColor = true;
-            // 
-            // button7
-            // 
-            this.button7.Font = new System.Drawing.Font("Segoe UI", 14.25F);
-            this.button7.Location = new System.Drawing.Point(151, 123);
-            this.button7.Name = "button7";
-            this.button7.Size = new System.Drawing.Size(27, 32);
-            this.button7.TabIndex = 10;
-            this.button7.Text = "Save";
-            this.button7.UseVisualStyleBackColor = true;
-            // 
-            // button8
-            // 
-            this.button8.Font = new System.Drawing.Font("Segoe UI", 14.25F);
-            this.button8.Location = new System.Drawing.Point(119, 120);
-            this.button8.Name = "button8";
-            this.button8.Size = new System.Drawing.Size(27, 32);
-            this.button8.TabIndex = 11;
-            this.button8.Text = "Save";
-            this.button8.UseVisualStyleBackColor = true;
-            // 
-            // button9
-            // 
-            this.button9.Font = new System.Drawing.Font("Segoe UI", 14.25F);
-            this.button9.Location = new System.Drawing.Point(69, 118);
-            this.button9.Name = "button9";
-            this.button9.Size = new System.Drawing.Size(27, 32);
-            this.button9.TabIndex = 12;
-            this.button9.Text = "Save";
-            this.button9.UseVisualStyleBackColor = true;
-            // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(12, 155);
+            this.label3.Font = new System.Drawing.Font("Yu Mincho", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(4, 156);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(72, 32);
+            this.label3.Size = new System.Drawing.Size(79, 31);
             this.label3.TabIndex = 13;
             this.label3.Text = "Term:";
             // 
             // term
             // 
+            this.term.Font = new System.Drawing.Font("Yu Mincho", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.term.Location = new System.Drawing.Point(10, 190);
             this.term.MinimumSize = new System.Drawing.Size(343, 80);
             this.term.Multiline = true;
@@ -198,15 +110,16 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(6, 273);
+            this.label4.Font = new System.Drawing.Font("Yu Mincho", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(4, 272);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(125, 32);
+            this.label4.Size = new System.Drawing.Size(130, 31);
             this.label4.TabIndex = 15;
             this.label4.Text = "Definition:";
             // 
             // definition
             // 
+            this.definition.Font = new System.Drawing.Font("Yu Mincho", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.definition.Location = new System.Drawing.Point(10, 306);
             this.definition.MinimumSize = new System.Drawing.Size(343, 80);
             this.definition.Multiline = true;
@@ -217,24 +130,26 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("Segoe UI", 20.25F);
+            this.label5.Font = new System.Drawing.Font("Yu Mincho", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
             this.label5.Location = new System.Drawing.Point(403, 120);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(236, 37);
+            this.label5.Size = new System.Drawing.Size(240, 35);
             this.label5.TabIndex = 18;
             this.label5.Text = "Flashcards in deck:";
             // 
             // listBox1
             // 
+            this.listBox1.Font = new System.Drawing.Font("Yu Mincho", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 14;
             this.listBox1.Location = new System.Drawing.Point(403, 155);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(273, 173);
+            this.listBox1.Size = new System.Drawing.Size(273, 172);
             this.listBox1.TabIndex = 19;
             // 
             // addCard
             // 
-            this.addCard.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.addCard.Font = new System.Drawing.Font("Yu Mincho", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.addCard.Location = new System.Drawing.Point(403, 333);
             this.addCard.Name = "addCard";
             this.addCard.Size = new System.Drawing.Size(273, 27);
@@ -245,7 +160,7 @@
             // 
             // removeCard
             // 
-            this.removeCard.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.removeCard.Font = new System.Drawing.Font("Yu Mincho", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.removeCard.Location = new System.Drawing.Point(403, 365);
             this.removeCard.Name = "removeCard";
             this.removeCard.Size = new System.Drawing.Size(273, 26);
@@ -258,39 +173,11 @@
             // 
             this.openFileDialog1.FileName = "openFileDialog1";
             // 
-            // comboBox1
-            // 
-            this.comboBox1.DropDownHeight = 200;
-            this.comboBox1.Font = new System.Drawing.Font("Segoe UI", 12F);
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.IntegralHeight = false;
-            this.comboBox1.Items.AddRange(new object[] {
-            "9",
-            "10",
-            "11",
-            "12",
-            "14",
-            "16",
-            "18",
-            "20",
-            "22",
-            "24",
-            "26",
-            "28",
-            "30",
-            "36",
-            "40"});
-            this.comboBox1.Location = new System.Drawing.Point(10, 123);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(37, 29);
-            this.comboBox1.TabIndex = 22;
-            // 
             // CreateFlashcard
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(688, 398);
-            this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.removeCard);
             this.Controls.Add(this.addCard);
             this.Controls.Add(this.listBox1);
@@ -299,14 +186,6 @@
             this.Controls.Add(this.label4);
             this.Controls.Add(this.term);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.button9);
-            this.Controls.Add(this.button8);
-            this.Controls.Add(this.button7);
-            this.Controls.Add(this.button6);
-            this.Controls.Add(this.button5);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.loadDeck);
             this.Controls.Add(this.label1);
@@ -331,14 +210,6 @@
         private Label label1;
         private Button loadDeck;
         private Label label2;
-        private Button button2;
-        private Button button3;
-        private Button button4;
-        private Button button5;
-        private Button button6;
-        private Button button7;
-        private Button button8;
-        private Button button9;
         private Label label3;
         private TextBox term;
         private Label label4;
@@ -349,6 +220,5 @@
         private Button removeCard;
         private OpenFileDialog openFileDialog1;
         private SaveFileDialog saveFileDialog1;
-        private ComboBox comboBox1;
     }
 }
